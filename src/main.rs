b10e3b32898294f35c58e4314fc0d0a7720b8ce6
Shogoro Yoshida <shogoro.yoshida@gmail.com>
2023-05-29 00:56:01 +0900

    ✨ Update to multi thread

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::io::prelude::*;
 use std::io::Write;
 use std::net::TcpListener;
 use std::net::TcpStream;
+use std::thread::spawn;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -10,26 +11,30 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
-        match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+        spawn(move || {
+            match stream {
+                Ok(mut _stream) => {
+                    println!("accepted new connection");
 
-                handle_connection(_stream)
-            }
-            Err(e) => {
-                println!("error: {}", e);
-            }
-        }
+                    loop {
+                        handle_connection(&mut _stream);
+                    }
+                }
+                Err(e) => {
+                    println!("error: {}", e);
+                }
+            };
+        });
     }
 }
 
-fn handle_connection(mut stream: TcpStream) {
+fn handle_connection(stream: &mut TcpStream) {
     let mut buffer = [0; 1024];
 
-    stream.read(&mut buffer).unwrap();
+    let _data = stream.read(&mut buffer);
 
     let response = "+PONG\r\n";
 
-    stream.write(response.as_bytes()).unwrap();
+    let _result = stream.write(response.as_bytes());
     stream.flush().unwrap();
 }
